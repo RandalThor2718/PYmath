495ae1e5d186c7412a04badd9cf36e0da5d21983
Henry-Gish <gishhenry781@gmail.com>
2026-08-14 19:50:51 -0500

    Enhance fibonacci.py with comments
    Added comments to clarify the purpose of the code.

--- fibbonacci.py
+++ fibbonacci.py
@@ -1,10 +1,13 @@
 upperbound = int(input("upperbound"))
+# sets an upper limit for n in f(n) where f is the fibonacci function
 
 def fibonacci(integer):
   if integer <= 1:
     return integer
   else:
     return fibonacci(integer-1) + fibonacci(integer-2)
+    # computes fibonacci sequence
 
 for i in range(1,upperbound):
   print(fibonacci(i))
+# executes fibonacci
